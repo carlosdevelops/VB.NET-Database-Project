bb759eac828412fbfb61c4227683f8d1660ec454
Carlos <carloszermeno1@gmail.com>
2020-11-13 23:11:08 -0600

    Add files via upload

---
+++ Carlos's Database.Designer.vb
@@ -0,0 +1,357 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.Table1BindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.DatabaseDataSet = New VB_Project.DatabaseDataSet()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtDescription = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.dateDue = New System.Windows.Forms.DateTimePicker()
+        Me.btnPrev = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.txtLocation = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.WorkOrderDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.LocationDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.NameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.EmailDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DescriptionDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DateDueDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Table1TableAdapter = New VB_Project.DatabaseDataSetTableAdapters.Table1TableAdapter()
+        Me.Label6 = New System.Windows.Forms.Label()
+        CType(Me.Table1BindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DatabaseDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(41, 254)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Name"
+        '
+        'txtName
+        '
+        Me.txtName.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.Table1BindingSource, "Name", True))
+        Me.txtName.Location = New System.Drawing.Point(207, 251)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(192, 31)
+        Me.txtName.TabIndex = 1
+        '
+        'Table1BindingSource
+        '
+        Me.Table1BindingSource.DataMember = "Table1"
+        Me.Table1BindingSource.DataSource = Me.DatabaseDataSet
+        '
+        'DatabaseDataSet
+        '
+        Me.DatabaseDataSet.DataSetName = "DatabaseDataSet"
+        Me.DatabaseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'txtEmail
+        '
+        Me.txtEmail.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.Table1BindingSource, "Email", True))
+        Me.txtEmail.Location = New System.Drawing.Point(207, 313)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(192, 31)
+        Me.txtEmail.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(41, 316)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(65, 25)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Email"
+        '
+        'txtDescription
+        '
+        Me.txtDescription.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.Table1BindingSource, "Description", True))
+        Me.txtDescription.Location = New System.Drawing.Point(207, 367)
+        Me.txtDescription.Name = "txtDescription"
+        Me.txtDescription.Size = New System.Drawing.Size(192, 31)
+        Me.txtDescription.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(41, 373)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(120, 25)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Description"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(41, 428)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(102, 25)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Due Date"
+        '
+        'dateDue
+        '
+        Me.dateDue.DataBindings.Add(New System.Windows.Forms.Binding("Value", Me.Table1BindingSource, "Date Due:", True))
+        Me.dateDue.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dateDue.Location = New System.Drawing.Point(207, 423)
+        Me.dateDue.Name = "dateDue"
+        Me.dateDue.Size = New System.Drawing.Size(192, 31)
+        Me.dateDue.TabIndex = 8
+        Me.dateDue.Value = New Date(2020, 11, 13, 0, 0, 0, 0)
+        '
+        'btnPrev
+        '
+        Me.btnPrev.Location = New System.Drawing.Point(46, 512)
+        Me.btnPrev.Name = "btnPrev"
+        Me.btnPrev.Size = New System.Drawing.Size(115, 48)
+        Me.btnPrev.TabIndex = 9
+        Me.btnPrev.Text = "Previous"
+        Me.btnPrev.UseVisualStyleBackColor = True
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(176, 512)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(97, 48)
+        Me.btnAdd.TabIndex = 10
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnNext
+        '
+        Me.btnNext.Location = New System.Drawing.Point(290, 512)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(109, 48)
+        Me.btnNext.TabIndex = 11
+        Me.btnNext.Text = "Next"
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(290, 588)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(109, 48)
+        Me.btnClose.TabIndex = 14
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(176, 588)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(97, 48)
+        Me.btnDelete.TabIndex = 13
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(46, 588)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(115, 48)
+        Me.btnSave.TabIndex = 12
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'txtLocation
+        '
+        Me.txtLocation.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.Table1BindingSource, "Location", True))
+        Me.txtLocation.Location = New System.Drawing.Point(207, 191)
+        Me.txtLocation.Name = "txtLocation"
+        Me.txtLocation.Size = New System.Drawing.Size(192, 31)
+        Me.txtLocation.TabIndex = 16
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(41, 194)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(94, 25)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "Location"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 166)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(445, 486)
+        Me.GroupBox1.TabIndex = 17
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Input"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.DataGridView1)
+        Me.GroupBox2.Location = New System.Drawing.Point(523, 166)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(907, 486)
+        Me.GroupBox2.TabIndex = 18
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Database"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.WorkOrderDataGridViewTextBoxColumn, Me.LocationDataGridViewTextBoxColumn, Me.NameDataGridViewTextBoxColumn, Me.EmailDataGridViewTextBoxColumn, Me.DescriptionDataGridViewTextBoxColumn, Me.DateDueDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataSource = Me.Table1BindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(6, 25)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 33
+        Me.DataGridView1.Size = New System.Drawing.Size(886, 461)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'WorkOrderDataGridViewTextBoxColumn
+        '
+        Me.WorkOrderDataGridViewTextBoxColumn.DataPropertyName = "Work Order"
+        Me.WorkOrderDataGridViewTextBoxColumn.HeaderText = "Work Order"
+        Me.WorkOrderDataGridViewTextBoxColumn.Name = "WorkOrderDataGridViewTextBoxColumn"
+        '
+        'LocationDataGridViewTextBoxColumn
+        '
+        Me.LocationDataGridViewTextBoxColumn.DataPropertyName = "Location"
+        Me.LocationDataGridViewTextBoxColumn.HeaderText = "Location"
+        Me.LocationDataGridViewTextBoxColumn.Name = "LocationDataGridViewTextBoxColumn"
+        '
+        'NameDataGridViewTextBoxColumn
+        '
+        Me.NameDataGridViewTextBoxColumn.DataPropertyName = "Name"
+        Me.NameDataGridViewTextBoxColumn.HeaderText = "Name"
+        Me.NameDataGridViewTextBoxColumn.Name = "NameDataGridViewTextBoxColumn"
+        '
+        'EmailDataGridViewTextBoxColumn
+        '
+        Me.EmailDataGridViewTextBoxColumn.DataPropertyName = "Email"
+        Me.EmailDataGridViewTextBoxColumn.HeaderText = "Email"
+        Me.EmailDataGridViewTextBoxColumn.Name = "EmailDataGridViewTextBoxColumn"
+        '
+        'DescriptionDataGridViewTextBoxColumn
+        '
+        Me.DescriptionDataGridViewTextBoxColumn.DataPropertyName = "Description"
+        Me.DescriptionDataGridViewTextBoxColumn.HeaderText = "Description"
+        Me.DescriptionDataGridViewTextBoxColumn.Name = "DescriptionDataGridViewTextBoxColumn"
+        '
+        'DateDueDataGridViewTextBoxColumn
+        '
+        Me.DateDueDataGridViewTextBoxColumn.DataPropertyName = "Date Due:"
+        Me.DateDueDataGridViewTextBoxColumn.HeaderText = "Date Due:"
+        Me.DateDueDataGridViewTextBoxColumn.Name = "DateDueDataGridViewTextBoxColumn"
+        '
+        'Table1TableAdapter
+        '
+        Me.Table1TableAdapter.ClearBeforeFill = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Castellar", 19.875!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(1, 42)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(1448, 64)
+        Me.Label6.TabIndex = 19
+        Me.Label6.Text = "Carlos Zermeno-Sample Database Project"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1442, 682)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.txtLocation)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnNext)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.btnPrev)
+        Me.Controls.Add(Me.dateDue)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtDescription)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Database Project- Carlos Zermeno"
+        CType(Me.Table1BindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DatabaseDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox2.ResumeLayout(False)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtDescription As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents dateDue As DateTimePicker
+    Friend WithEvents btnPrev As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnNext As Button
+    Friend WithEvents btnClose As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents txtLocation As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents DatabaseDataSet As DatabaseDataSet
+    Friend WithEvents Table1BindingSource As BindingSource
+    Friend WithEvents Table1TableAdapter As DatabaseDataSetTableAdapters.Table1TableAdapter
+    Friend WithEvents WorkOrderDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents LocationDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents NameDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents EmailDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents DescriptionDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents DateDueDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents Label6 As Label
+End Class
---
+++ Carlos's Database.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Table1BindingSource.AddNew()
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'DatabaseDataSet.Table1' table. You can move, or remove it, as needed.
+        Me.Table1TableAdapter.Fill(Me.DatabaseDataSet.Table1)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnPrev.Click
+        Table1BindingSource.MovePrevious()
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnNext.Click
+        Table1BindingSource.MoveNext()
+
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        On Error GoTo SaveErr
+        Table1BindingSource.EndEdit()
+        Table1TableAdapter.Update(DatabaseDataSet.Table1)
+        MessageBox.Show("Saved")
+
+
+SaveErr:
+        Exit Sub
+
+
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+
+        Table1BindingSource.RemoveCurrent()
+
+
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+End Class
